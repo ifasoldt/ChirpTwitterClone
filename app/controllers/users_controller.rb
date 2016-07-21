7cfd7b57d8236ae70f7d6fb30f907de392961576
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-07-21 16:19:14 -0400

    changed user params to not require :user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       render json: @user, status: :created, location: @user
     else
@@ -61,6 +61,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :password, :email, :picture_url)
+      params.permit(:name, :password, :email, :picture_url)
     end
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,20 @@
 # This file is used by Rack-based servers to start the application.
+require ::File.expand_path('../config/environment',  __FILE__)
+run Rails.application
+
+require 'rack/cors'
+use Rack::Cors do
 
 require_relative 'config/environment'
 
 run Rails.application
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
