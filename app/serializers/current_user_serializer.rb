269acdfd6ac7cc8a0dd74c716a5f5930563e4b84
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-07-22 17:19:30 -0400

    token added

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -1,7 +1,7 @@
 class CurrentUserSerializer < ActiveModel::Serializer
 
-  #new user serializer that only returns token. This will go to user controller timeline.
-  attributes :id, :name, :email, :picture_url, :followers, :followees, :followers_count, :followees_count, :suggested_people_to_follow
+  #new user serializer that only returns token. This will go to user controller timeline. Timeline needs to be included.
+  attributes :id, :name, :token, :email, :picture_url, :followers, :followees, :followers_count, :followees_count, :suggested_people_to_follow
 
   def followers
     object.followers(User)
